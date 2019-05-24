2640e26b5155f56f80f424ddab45259042e50bcb
RafaelSolorzano326 <50973930+RafaelSolorzano326@users.noreply.github.com>
2019-05-23 20:10:33 -0600

    Subir archivos
    se subieron los archivos de pruebas con la primera grafica por defecto que luego sera cambiada

---
+++ graficas.js
@@ -0,0 +1,20 @@
+
+new Morris.Line({
+  // ID of the element in which to draw the chart.
+  element: 'myfirstchart',
+  // Chart data records -- each entry in this array corresponds to a point on
+  // the chart.
+  data: [
+    { year: '2008', value: 20 },
+    { year: '2009', value: 10 },
+    { year: '2010', value: 5 },
+    { year: '2011', value: 5 },
+    { year: '2012', value: 20 }
+  ],
+  xkey: 'year',
+  ykeys: ['value'],
+  labels: ['Value'],
+  resize: true,
+  lineColors:['green']
+});
+
